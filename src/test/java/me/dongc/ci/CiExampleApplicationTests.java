b0a684d018fa1fc7893efdfc532c3fd6f204d7c8
Dongd <choise154@gmail.com>
2019-10-18 11:23:34 +0900

    first comit

---
+++ src/test/java/me/dongc/ci/CiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dongc.ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
